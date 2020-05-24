bcffa733a10eb73265ac24ed6c3a3cbe76bb314a
Dai-jl <Dai_jl@outlook.com>
2020-05-24 18:16:45 +0800

    djl update

--- club_front/src/router/index.js
+++ club_front/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Leader from '../view/leader/Leader'
+import Admin from '../view/admin/Admin'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/leader',
+      name: 'Leader',
+      component: Leader
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
     }
   ]
 })
